ce84c4f81eb388ecdb9b0dd10ce0d5a62708afc7
putiyeb <putiyeb@lillemap.net>
2018-08-27 16:40:19 +0800

    xml constructor injection: collection

--- src/soundsystem/BlackDisc.java
+++ src/soundsystem/BlackDisc.java
@@ -1,16 +1,23 @@
 package soundsystem;
 
+import java.util.List;
+
 public class BlackDisc implements CompactDisc {
 
     private String title;
     private String artist;
+    private List<String> tracks;
 
-    public BlackDisc(String title, String artist){
+    public BlackDisc(String title, String artist, List<String> tracks){
         this.title = title;
         this.artist = artist;
+        this.tracks = tracks;
     }
 
     public void play(){
         System.out.println("Playing " + title + " by " + artist);
+        for(String track: tracks){
+            System.out.println("-Track: " + track);
+        }
     }
 }
